d24927d530703852e496e04db4e68c4b3abfa16e
César Rodrigues de Oliveira <cesar.rso@hotmail.com>
2023-09-19 13:17:42 -0300

    Changing asgi settings

--- FoodServer/routing.py
+++ FoodServer/routing.py
@@ -1,12 +1,16 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 import orders.routing
 
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(AllowedHostsOriginValidator(
         URLRouter(
             orders.routing.websocket_urlpatterns
         )
+    )
     ),
 })
