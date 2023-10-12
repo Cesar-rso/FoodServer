67ad933ca2020d73486584e5d9f786b7df11e197
César Rodrigues de Oliveira <cesar.rso@hotmail.com>
2023-10-12 20:35:43 -0300

    Fixed errors in javascript, added serializers to consumers

--- orders/consumers.py
+++ orders/consumers.py
@@ -1,5 +1,7 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.db import database_sync_to_async
 from .models import Orders
+from .serializers import OrderSerializer
 from datetime import datetime
 
 
@@ -9,13 +11,16 @@ class OrdersConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
 
         await self.accept()
+        content = {"username": "system", "message": str(datetime.today().year) + "-" + str(datetime.today().month) + "-" + str(datetime.today().day)}
+        await self.receive_json(content)
 
 
     async def receive_json(self, content):
         
+        print("Receiving message...")
         date_msg = datetime.strptime(content['message'], '%Y-%m-%d')
         username = content['username']
-        message = Orders.objects.filter(date__gt=date_msg)
+        message = await get_Orders(date_msg)
 
         await self.send_json(
             {
@@ -25,3 +30,14 @@ class OrdersConsumer(AsyncJsonWebsocketConsumer):
             }
         )
 
+
+@database_sync_to_async
+def get_Orders(date: datetime) -> dict:
+
+    orders = Orders.objects.filter(date__gt=date)
+    if orders:
+        message = OrderSerializer(orders).data
+    else:
+        message = {}
+
+    return message
