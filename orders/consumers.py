991bd76b4d25948a03777f1ceca18d9cb7b1a088
César Rodrigues de Oliveira <cesar.rso@hotmail.com>
2023-11-14 10:02:41 -0300

    Update consumers.py
    Fixed bug on displaying items name and price on orders page

--- orders/consumers.py
+++ orders/consumers.py
@@ -1,6 +1,6 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
-from .models import Orders
+from .models import Orders, Products
 from .serializers import OrderSerializer
 from datetime import datetime
 
@@ -38,8 +38,15 @@ class OrdersConsumer(AsyncJsonWebsocketConsumer):
 def get_Orders(date: datetime) -> dict:
 
     orders = Orders.objects.filter(date__gt=date).order_by("date")
+    prod_details = []
     if len(orders) > 0:
         message = OrderSerializer(orders[0]).data
+        for item in message["product"]:
+            product = Products.objects.get(pk=item)
+            prod_details.append({"id": product.id, "name": product.name, "price": product.price})
+
+        message["product"] = prod_details
+
     else:
         message = {}
 
