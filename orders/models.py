858fe82388df421aab65db085eb7dd946fa76874
César Rodrigues de Oliveira <cesar.rso@hotmail.com>
2024-02-14 10:50:15 -0300

    Added company info model

--- orders/models.py
+++ orders/models.py
@@ -91,4 +91,8 @@ class Waiters(models.Model):
     )
 
 
-
+class Company(models.Model):
+    name = models.CharField(max_length=200, default='')
+    address = models.CharField(max_length=400, default='')
+    phone = models.IntegerField(default=0000000000)
+    logo = models.ImageField(upload_to='company/', default='default_logo.jpg')
